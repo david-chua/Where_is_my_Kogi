c6c6c5d67aba30a652a016837a1d27b4dadb477f
dp_chua <dp_chua@hotmail.com>
2017-01-24 19:17:19 -0500

    created log-in session to delete session after refresh

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,17 @@ router = express.Router();
 var User = require('../models/user.js');
 var authHelpers = require('../helpers/auth.js');
 
-
+//Show page to show all the users in the same page;
+router.get('/', function(req, res){
+  User.find({})
+  .exec(function(err, users){
+    if (err) {console.log(err); }
+    res.render('users/index.hbs', {
+      users: users,
+      currentUser: req.session.currentUser })
+    })
+  })
+})
 
 
 
