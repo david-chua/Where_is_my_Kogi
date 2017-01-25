fdfa9380792efe99ee3a761d8619a78f88e0f35e
dp_chua <dp_chua@hotmail.com>
2017-01-24 19:22:40 -0500

    created sign up to send user to sign up form and a show route

--- controllers/users.js
+++ controllers/users.js
@@ -11,10 +11,23 @@ router.get('/', function(req, res){
     res.render('users/index.hbs', {
       users: users,
       currentUser: req.session.currentUser })
-    })
-  })
-})
+    });
+  });
 
+// Signup function
+router.get('/signup', function(req,res){
+  res.render('users/signup.hbs');
+});
+
+//showing the user's page to show only his information
+router.get('/:id', authHelpers.authorize, function(req, res){
+  User.findById(req.params.id)
+  .exec(function(err, user){
+    if (err) {res.send("Oops! You are not authorized!")};
+    console.log(user);
+    res.render('user/show.hbs', { user: user });
+  });
+});
 
 
 
